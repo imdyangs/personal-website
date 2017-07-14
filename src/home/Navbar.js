1cedd363db2cc2ea6f1ea291880f59bc3da1c08b
imdyangs <iamdyangster@gmail.com>
2017-07-14 00:44:27 -0400

    updated icon, sizing, added react router, fonts

--- src/home/Navbar.js
+++ src/home/Navbar.js
@@ -1,11 +1,18 @@
 import React from "react";
-import {TweenMax} from "gsap";
+import logo from "./logoNoDsvg.svg";
+
+const imageStyles = {
+  paddingTop: '8px',
+  width: '44px',
+  height: '44px'
+}
 
 class Navbar extends React.Component {
   render() {
     return (
       <div className="navbar">
         <div className="leftNav">
+          <img src={logo} alt="" style={imageStyles}/>
           <a href="/" className="contact">
             {this.props.isMobile ? <div>david</div> : <div>david yang</div>}
           </a>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import About from './home/About';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 import './index.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
