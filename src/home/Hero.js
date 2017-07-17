a00ea1c2202333037cd87420a13e4a3f80a8659e
imdyangs <iamdyangster@gmail.com>
2017-07-16 22:49:37 -0400

    finished routing

--- src/home/Hero.js
+++ src/home/Hero.js
@@ -1,61 +1,139 @@
-import React from "react";
-import { Power2, TimelineLite, Power4 } from "gsap";
+import React from 'react';
+import { Power2, TimelineLite, TweenMax } from 'gsap';
 
-import Navbar from "./Navbar.js";
-import "./Hero.css";
+import Navbar from './Navbar.js';
+import './Hero.css';
 
 class Hero extends React.Component {
+  state = {
+    page: 'home'
+  };
+
+  constructor(props) {
+    super(props);
+    this.contact = this.contact.bind(this);
+    this.about = this.about.bind(this);
+    this.home = this.home.bind(this);
+  }
+
+  contact(e) {
+    TweenMax.to('.home', 0.4, { opacity: 0 });
+    TweenMax.to('.aboutPage', 0.4, { opacity: 0 });
+    setTimeout(() => {
+      this.setState({
+        page: 'contact'
+      });
+      TweenMax.to('.contactPage', 0.4, { opacity: 1 });
+    }, 420);
+  }
+
+  about(e) {
+    TweenMax.to('.home', 0.4, { opacity: 0 });
+    TweenMax.to('.contactPage', 0.4, { opacity: 0 });
+    setTimeout(() => {
+      this.setState({
+        page: 'about'
+      });
+      TweenMax.to('.aboutPage', 0.4, { opacity: 1 });
+    }, 420);
+  }
+
+  home(e) {
+    TweenMax.to('.aboutPage', 0.4, { opacity: 0 });
+    TweenMax.to('.contactPage', 0.4, { opacity: 0 });
+    setTimeout(() => {
+      console.log('ok')
+      this.setState({
+        page: 'home'
+      });
+      TweenMax.to('.home', 0.4, { opacity: 1 });
+    }, 420);
+  }
+
   componentDidMount() {
     const aboutMeTimeline = new TimelineLite();
     if (this.props.isMobile) {
       aboutMeTimeline
-        .from("#one", 1.2, {opacity:0, delay:0.8, ease:Power2.easeIn})
-        .from("#two", 1.2, {opacity:0, ease:Power2.easeIn}, "+=0.1")
-        .from("#three", 0.8, {opacity:0}, "+=0.6")
-        .from("#four", 0.8, {opacity:0}, "+=0.4")
+        .from('#one', 1.2, { opacity: 0, delay: 0.8, ease: Power2.easeIn })
+        .from('#two', 1.2, { opacity: 0, ease: Power2.easeIn }, '+=0.1')
+        .from('#three', 0.8, { opacity: 0 }, '+=0.6')
+        .from('#four', 0.8, { opacity: 0 }, '+=0.4');
     } else {
       aboutMeTimeline
-        .from("#one", 1.1, {opacity:0, delay:0.8, ease:Power2.easeIn})
-        .from("#two", 1.1, {opacity:0, ease:Power2.easeIn}, "-=0.3")
-        .from("#three", 1.1, {opacity:0, ease:Power2.easeIn})
-        .from("#four", 1.1, {opacity:0, ease:Power2.easeIn}, "-=0.4")
-        .from("#five", 0.7, {opacity:0}, "+=0.2")
-        .from("#six", 0.7, {opacity:0}, "+=0.2")
-        .from("#seven", 0.7, {opacity:0}, "+=0.2")
+        .from('#one', 1, { opacity: 0, delay: 0.8, ease: Power2.easeIn })
+        .from('#two', 1, { opacity: 0, ease: Power2.easeIn }, '-=0.4')
+        .from('#three', 1, { opacity: 0, ease: Power2.easeIn })
+        .from('#four', 1, { opacity: 0, ease: Power2.easeIn }, '-=0.4')
+        .from('#five', 0.7, { opacity: 0 }, '+=0.1')
+        .from('#six', 0.7, { opacity: 0 }, '+=0.1')
+        .from('#seven', 0.7, { opacity: 0 }, '+=0.1');
     }
   }
   render() {
+    let body = null;
+    if (this.state.page === 'home') {
+      if (this.props.isMobile) {
+        body = (
+          <div className="home">
+            <span id="one">my name is <br /> david yang</span>
+            {' '}
+            <br />
+            <span id="two">and i aspire to <br /> build scalable,</span>
+            {' '}
+            <br />
+            <span id="three"> world positive</span>
+            {' '}
+            <span id="four">technologies.</span>
+          </div>
+        );
+      } else {
+        body = (
+          <div className="home">
+            <span id="one">my name is</span>
+            <span id="two"> david yang</span>
+            {' '}
+            <span id="three"> and i aspire</span>
+            {' '}
+            <span id="four"> to build </span>
+            <br />
+            <span id="five">scalable,</span>
+            {' '}
+            <span id="six">world positive </span>
+            <span id="seven">technologies.</span>
+          </div>
+        );
+      }
+    }
+
+    if (this.state.page === 'about') {
+      console.log('about');
+      body = (
+        <div className="aboutPage">
+          okay
+        </div>
+      );
+    }
+
+    if (this.state.page === 'contact') {
+      console.log('contact');
+      body = (
+        <div className="contactPage">
+          litty
+        </div>
+      )
+    }
+
     return (
       <div className="container">
-        <Navbar isMobile={this.props.isMobile} />
+        <Navbar
+          isMobile={this.props.isMobile}
+          handleContact={this.contact}
+          handleHome={this.home}
+          handleAbout={this.about}
+        />
         <div className="content">
           <div className="about-me">
-            {/*Line Breaks for Web and Mobile*/}
-            {this.props.isMobile
-              ? <div>
-                  <span id="one">my name is <br /> david yang</span>
-                  {" "}
-                  <br />
-                  <span id="two">and i aspire to <br /> build scalable,</span>
-                  {" "}
-                  <br />
-                  <span id="three"> world positive</span>
-                  {" "}
-                  <span id="four">technologies.</span>
-                </div>
-              : <div>
-                  <span id="one">my name is</span>
-                  <span id="two"> david yang</span>
-                  {" "}
-                  <span id="three"> and i aspire</span>
-                  {" "}
-                  <span id="four"> to build </span>
-                  <br />
-                  <span id="five">scalable,</span>
-                  {" "}
-                  <span id="six">world positive </span>
-                  <span id="seven">technologies.</span>
-                </div>}
+            {body}
           </div>
         </div>
       </div>
--- src/home/Navbar.js
+++ src/home/Navbar.js
@@ -1,25 +1,44 @@
-import React from "react";
-import logo from "./logoNoDsvg.svg";
+import React from 'react';
+import logo from './logoNoDsvg.svg';
 
 const imageStyles = {
   paddingTop: '4px',
   width: '44px',
   height: '46px'
-}
+};
 
 class Navbar extends React.Component {
+  onHome = e => {
+    e.preventDefault();
+    this.props.handleHome();
+  };
+
+  onAbout = e => {
+    e.preventDefault();
+    this.props.handleAbout();
+  };
+
+  onContact = e => {
+    e.preventDefault();
+    this.props.handleContact();
+  };
+
   render() {
     return (
       <div className="navbar">
         <div className="leftNav">
-          <img src={logo} alt="" style={imageStyles}/>
-          <a href="/" className="contact">
+          <a href="/" onClick={this.onHome}>
+            <img src={logo} alt="" style={imageStyles} />
+          </a>
+          <a href="/" className="contact" onClick={this.onHome}>
             {this.props.isMobile ? <div>david</div> : <div>david yang</div>}
           </a>
         </div>
         <div className="rightNav">
-          <a href="/about" className="about">about</a>
-          <a href="/projects" className="projects">projects</a>
+          <a href="/about" className="about" onClick={this.onAbout}>about</a>
+          <a href="/projects" className="projects" onClick={this.onContact}>
+            contact
+          </a>
         </div>
       </div>
     );
